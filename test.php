a1d2e2b5561076a3b88ffb3c6a9eff41d5731b12
bai9011 <1624863032@qq.com>
2018-05-04 19:30:27 +0800

    baidu-test

---
+++ test.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of test
+ *
+ * @author Yuki
+ */
+class test {
+    //put your code here
+    
+    
+
+//创建浏览器
+//$driver=
+// RemoteWebDriver::create('http://localhost:4444/wd/hub',$capabilities);
+//$driver->get('http://www.baidu.com/');
+////// adding cookie
+////$driver->manage()->deleteAllCookies();
+////$cookie = new Cookie('cookie_name', 'cookie_value');
+////$driver->manage()->addCookie($cookie);
+////$cookies = $driver->manage()->getCookies();
+////print_r($cookies);
+////echo 123;
+////// click the link 'About'
+//$ser = $driver->findElement(WebDriverBy::id('kw'));
+//$ser->sendKeys('php');
+////    //自动弹出方法:ActionChains(browser).move_to_element(article).perform()  
+////        ActionChains($driver)->
+////sleep(3);
+////$baiduchax = $driver->findElement(WebDriverBy::id('su'));
+////$driver->WebDriverAction($baiduchax->click())->perform();
+//sleep(3);
+//$driver->findElement(WebDriverBy::className("bg s_btn"))->click();  
+//sleep(10);
+//// wait until the page is loaded
+//echo 456;
+//$driver->wait()->until(
+//    WebDriverExpectedCondition::titleContains('123')
+//);
+//echo 777;
+//// print the title of the current page
+//echo "The title is '" . $driver->getTitle() . "'\n";
+//// print the URI of the current page
+//echo "The current URI is '" . $driver->getCurrentURL() . "'\n";
+//// write 'php' in the search box
+//$driver->findElement(WebDriverBy::id('q'))
+//    ->sendKeys('php');
+//// submit the form
+//$driver->findElement(WebDriverBy::id('submit'))
+//    ->click(); // submit() does not work in Selenium 3 because of bug https://github.com/SeleniumHQ/selenium/issues/3398
+//// wait at most 10 seconds until at least one result is shown
+//$driver->wait(10)->until(
+//    WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(
+//        WebDriverBy::className('gsc-result')
+//    )
+//);
+//// close the Firefox
+//$driver->quit();
+
+    //$webDriver->findElement(WebDriverBy::id("su"))->click();
+////print_r($login_button);
+//if($login_button)
+//    {
+//    echo "dianji";
+//     echo 1;
+//     $webDriver->findElement(WebDriverBy::id('kw'))->sendKeys('wwe')->submit();
+//
+////        $login_button->sendKeys(3);
+////        $webDriver->findElement(WebDriverBy::xpath("//*@[id='su']"))->click();
+////        echo 2;
+//        $webDriver->findElement(WebDriverBy::id('kw'))->clear();
+//$webDriver->findElement(WebDriverBy::id('kw'))->sendKeys('123213')->submit();
+//$webDriver->findElement(WebDriverBy::xpath("//*@[id='su']"))->click();
+//        exit;
+//    //如果存在,进行登录操作
+//    $login_button->sendKeys("5555555");
+//        //->submit();
+//    $login_button->pressKey();
+//    
+//exit;
+////$login_button->click;
+//    $webDriver->wait(5);
+//            //->until(WebDriverExpectedCondition::urlContains("login"));
+//
+//    //自动弹出方法:ActionChains(browser).move_to_element(article).perform()  
+//    $login_url = $webDriver->getCurrentURL();
+//    if( "https://login.modian.com/u/login?login#m_login" == $login_url){
+//        //执行登录
+//        $login_password_button = $webDriver->findElement(WebDriverBy::classname("login-web go-login login-tit-right pad-width"));
+//        $login_password_button->click; 
+//        //赋值
+//       $user =  $webDriver->findElement(WebDriverBy::xpath("//dsahdhas/djsajd"));
+//       $password = $webDriver->findElement(WebDriverBy::id("password"));
+//       $user->sendKeys("15001364219");
+//       $password->sendKeys("111111");
+//       $recoverypass = $webDriver->findElement(WebDriverBy::id("recoverypass"));
+//       $recoverypass->click;
+//       $login_over_url = $webDriver->getCurrentURL();
+//       print_r($login_over_url);
+//       $right_url = $url;
+//       if($login_over_url===$right_url)  echo "login success";
+//    }
+//    
+//    
+//    }else{
+//        //如果不存在,判断是否处于已登录状态
+//        $is_login = $webDriver->findElement(WebDriverBy::id(""));
+//        if($is_login) {
+//            //已登录 执行退出操作
+//            
+//            
+//        }else{
+//            //错误
+//            error_log($message);
+//            print_r($expression);
+//        }
+//        
+//        }
+//        
+//
+////$webDriver.get_required_files();
+//var_dump($webDriver->getTitle());
+//
+    
+    
+}
